3148a7fa413db59f57b34bbbdea33e499f718658
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-11-09 11:23:44 +0700

    Add JSON Parser

--- app/src/main/java/com/example/pbb/JSONParserActivity.java
+++ app/src/main/java/com/example/pbb/JSONParserActivity.java
@@ -2,13 +2,91 @@ package com.example.pbb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.example.pbb.utils.HTTPHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JSONParserActivity extends AppCompatActivity {
-
+    private ListView lvContact;
+    ArrayList<HashMap<String, String>> contactList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jsonparser);
+
+        contactList = new ArrayList<>();
+        lvContact = findViewById(R.id.lvContact);
+        new GetContact().execute();
+    }
+
+    private class GetContact extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HTTPHandler sh = new HTTPHandler();
+            String url = "https://ppb.woofsie.xyz/contacts-from-api-androidhive-info.json";
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e("JSONParser", "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    // getting JSON array node
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+                    // looping through all contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+                        // phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+                        // temporary hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+                        // adding contact to contactList
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(JSONParserActivity.this, "JSON Data is being downloaded", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(JSONParserActivity.this, contactList, R.layout.json_parser_list_item,
+                    new String[] {"id", "name", "email", "mobile"},
+                    new int[] {R.id.tvID, R.id.tvName, R.id.tvEmail, R.id.tvMobile});
+            lvContact.setAdapter(adapter);
+        }
     }
 }
